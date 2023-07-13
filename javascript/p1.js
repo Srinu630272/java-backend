08dbc9e62d55e71d9083b096f96ff056fe5c38dd
MV Thanoshan <you@example.com>
2023-07-13 10:10:36 +0530

    adding html files

---
+++ javascript/p1.js
@@ -0,0 +1,28 @@
+
+const arr=[12,23,32,23,34]
+
+function myFunction(ele){
+    console.log(ele)
+    }
+    arr.forEach(myFunction)
+    
+    
+    let number = 0
+
+
+function average(ele){
+    
+    number=number+ele
+console.log(ele+ele)
+}
+arr.forEach(avg)
+function avg(ele){
+    average(ele)
+}
+console.log(number/5)
+
+
+
+
+
+
